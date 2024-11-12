135584372c760aaef5f443ca7da6e2ae21875a0d
vardhan barishetti <67987365+vardhan-barishetti@users.noreply.github.com>
2024-11-11 22:54:41 -0500

    intial commit

---
+++ src/main/java/com/spring/jdbc/ecom/SpringJdbcEcomApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jdbc.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcEcomApplication.class, args);
+	}
+
+}
